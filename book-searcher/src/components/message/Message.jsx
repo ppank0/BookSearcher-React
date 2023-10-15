360b2c8f4c952cf713ccde5d4d471f15117510b1
Pankova <pankkoo89@gmail.com>
2023-10-15 04:21:31 +0300

    feat: create message component

---
+++ book-searcher/src/components/message/Message.jsx
@@ -0,0 +1,14 @@
+import './message.css'
+import React, { Component } from 'react'
+// import bookImg from '../../img/book.jpg'
+
+class Message extends Component{
+    render(){
+        return (
+            <>
+                <p className="message-text">Nothing Found</p>
+            </>
+        )
+    }
+}
+export default Message;
